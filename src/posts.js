75446b87be478c6f1c1c802c108368378bab1f84
Jonathan <yoniyaniv@gmail.com>
2019-12-23 15:40:23 -0500

    ajax test

--- src/posts.js
+++ src/posts.js
@@ -1,8 +1,11 @@
-// $(document).ready(function(){
-//   $("#posts").append("<p>post 1...</p>");
-//   $("#posts").append("<p>post 2...</p>");
-// });
-
-//
-
-$( "#post-list" ).append( $( "ul" ).load( "https://jgyaniv.github.io/posts/post0001.html #post-title" ) )
+$.ajax({
+url: "https://jgyaniv.github.io/posts/post0001.html",
+cache: false
+})
+.done(function( html ) {
+  let $html = $(html);
+  let title = $html.find("#post-title").prop('outerHTML');
+  let $description = $html.find("#post-description");
+  $( "#posts" ).append( `<a href='https://jgyaniv.github.io/posts/post0001.html'>${title}</a>` );
+  $( "#posts" ).append( $description );
+});
