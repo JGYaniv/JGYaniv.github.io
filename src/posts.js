0b4ce4d89d4eaa080f9a824c509835eb61797b6f
Jonathan <yoniyaniv@gmail.com>
2019-12-23 12:36:44 -0500

    jquery load test

--- src/posts.js
+++ src/posts.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
-  $("#posts").append("<p>post 1...</p>");
-  $("#posts").append("<p>post 2...</p>");
-});
+// $(document).ready(function(){
+//   $("#posts").append("<p>post 1...</p>");
+//   $("#posts").append("<p>post 2...</p>");
+// });
+
+//
+
+$( "#post-list" ).append( $( "ul" ).load( "https://jgyaniv.github.io/posts/post0001.html #post-title" ) )
