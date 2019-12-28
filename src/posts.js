dc5e09a4ea72ca3dc62697c1599fcd26e3cb184c
Jonathan <yoniyaniv@gmail.com>
2019-12-28 12:16:13 -0500

    update addListItem function

--- src/posts.js
+++ src/posts.js
@@ -1,4 +1,4 @@
-let listUrl = "https://jgyaniv.github.io/posts/post-list.txt";
+let listUrl = "https://jgyaniv.github.io/posts/post-list.html";
 let samplePostUrl = "https://jgyaniv.github.io/posts/post0001.html";
 
 const addListItem = function(url){
@@ -6,16 +6,19 @@ const addListItem = function(url){
   url: url,
   cache: false
   })
-  .done(function( html ) {
+  .done(function(html) {
     let $html = $(html);
+    let url = samplePostUrl
     let title = $html.find("#post-title").prop('innerHTML');
     let description = $html.find("#post-description").prop('outerHTML');
     $( "#post-list" ).append(`<li id='listed-post'><a href='${url}'>${title}</a>\n${description}</li>`);
-  });
-}
+  })
+};
+
+let allPostFiles = ["post0001"]
 
 const compileUrl = function(title){
-  console.log(title);
+  return `https://jgyaniv.github.io/posts/${title}.html`
 }
 
-addListItem(samplePostUrl);
+allPostFiles.forEach((fileName) => addListItem(compileUrl(fileName)));
