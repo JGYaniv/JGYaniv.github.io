7ee96f13c36e0a4c126f1fb42513ae723839638b
Jonathan <yoniyaniv@gmail.com>
2019-12-28 11:45:30 -0500

    addListItem function added

--- src/posts.js
+++ src/posts.js
@@ -1,11 +1,21 @@
-$.ajax({
-url: "https://jgyaniv.github.io/posts/post0001.html",
-cache: false
-})
-.done(function( html ) {
-  let $html = $(html);
-  let title = $html.find("#post-title").prop('outerHTML');
-  let $description = $html.find("#post-description");
-  $( "#posts" ).append( `<a href='https://jgyaniv.github.io/posts/post0001.html'>${title}</a>` );
-  $( "#posts" ).append( $description );
-});
+let listUrl = "https://jgyaniv.github.io/posts/post-list.txt";
+let samplePostUrl = "https://jgyaniv.github.io/posts/post0001.html";
+
+const addListItem = function(url){
+  $.ajax({
+  url: url,
+  cache: false
+  })
+  .done(function( html ) {
+    let $html = $(html);
+    let title = $html.find("#post-title").prop('innerHTML');
+    let description = $html.find("#post-description").prop('outerHTML');
+    $( "#post-list" ).append(`<li id='listed-post'><a href='${url}'>${title}</a>\n${description}</li>`);
+  });
+}
+
+const compileUrl = function(title){
+  console.log(title);
+}
+
+addListItem(samplePostUrl);
