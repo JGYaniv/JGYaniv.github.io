5f7290f687545767995bd6f39a631092e0584e3c
Jonathan <yoniyaniv@gmail.com>
2020-02-27 13:19:10 -0500

    added sticky header and started portfolio section

--- src/navigation.js
+++ src/navigation.js
@@ -1,12 +1,36 @@
 // adds "selected" formatting to menu item when on page
 window.onhashchange = function() {
+  setSelected();
+}
+
+window.onload = function() {
+  setSelected();
+  var header = document.getElementById("navigation");
+  var sticky = header.offsetTop;
+  window.onscroll = function() {stickyHeader(sticky, header)};
+}
+
+const setSelected = function(){
   resetMenu();
   var pageName = window.location.hash;
   var menuItem = document.querySelectorAll(`[href="${pageName}"]`)[0];
-  menuItem.setAttribute("class","selected-menu-item");
+  if (menuItem.className === 'menu-item') {
+    menuItem.setAttribute("class","selected menu-item");
+  }
 }
 
 const resetMenu = function(){
-  var menuItems = document.querySelectorAll(`[class="selected-menu-item"]`);
+  var menuItems = document.querySelectorAll(`[class="selected menu-item"]`);
   menuItems.forEach(element => element.setAttribute("class", "menu-item"));
+}
+
+function stickyHeader(sticky, header) {
+  if (window.pageYOffset >= (sticky-10)) {
+    header.classList.add("sticky");
+    document.getElementById("content").setAttribute("style","margin-top:71px;");
+  } else {
+    console.log('unsticking')
+    header.classList.remove("sticky");
+    document.getElementById("content").setAttribute("style","margin-top:0;");
+  }
 }
