a5dbf29790275c6a5d6d7878f7b1cc7948ea08bc
Jonathan <yoniyaniv@gmail.com>
2020-03-11 20:06:45 -0400

    xhr navigation added

--- src/navigation.js
+++ src/navigation.js
@@ -1,46 +1,53 @@
-// adds "selected" formatting to menu item when on page
-window.onhashchange = function() {
-  setSelected();
-}
+const endpoint_url = {
+  "portfolio.html": "https://jgyaniv.github.io/posts/post0001.html",
+  "about.html": "https://jgyaniv.github.io/posts/post0001.html",
+  "blog.html": "https://jgyaniv.github.io/posts/post0001.html",
+  "about.html": "https://jgyaniv.github.io/posts/post0001.html"
+};
 
 window.onload = function() {
-  setSelected();
   var header = document.getElementById("navigation");
   var sticky = header.offsetTop;
   window.onscroll = function() {stickyHeader(sticky, header)};
 
-  // document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  //     anchor.addEventListener('click', function (e) {
-  //         e.preventDefault();
-  //
-  //         document.querySelector(this.getAttribute('href')).scrollIntoView({
-  //             behavior: 'smooth'
-  //         });
-  //     });
-  // });
+  var links = document.querySelectorAll("#navigation a")
+for( link of links) {link.addEventListener("click",
+          changepage.bind(null, link.getAttribute("href") )
+        );
+      }
 }
 
-const setSelected = function(){
-  resetMenu();
-  var pageName = window.location.hash;
-  var menuItem = document.querySelectorAll(`[href="${pageName}"]`)[0];
-  if (menuItem.className === 'menu-item') {
-    menuItem.setAttribute("class","selected menu-item");
-  }
+function changepage(endpoint, event){ event.preventDefault();
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", appendResponse);
+  xhr.open("GET",endpoint_url[ endpoint ]);
+  xhr.responseType = 'document';
+  xhr.send();
 }
 
-const resetMenu = function(){
-  var menuItems = document.querySelectorAll(`[class="selected menu-item"]`);
-  menuItems.forEach(element => element.setAttribute("class", "menu-item"));
+function appendResponse() {
+  var tempContent = this.responseXML.getElementById("post").innerHTML;
+  console.log("//// response/// ", tempContent);
+  document.querySelector(".page").innerHTML = tempContent;
 }
 
+// keeps header on top of page
 function stickyHeader(sticky, header) {
   if (window.pageYOffset >= (sticky-10)) {
     header.classList.add("sticky");
     document.getElementById("content").setAttribute("style","margin-top:71px;");
   } else {
-    console.log('unsticking')
     header.classList.remove("sticky");
     document.getElementById("content").setAttribute("style","margin-top:0;");
   }
 }
+
+
+//
+// function makeRequest( req_url ){
+//   var xhr = new XMLHttpRequest();
+//   xhr.addEventListener("load", appendResponse);
+//   xhr.open("GET","https://jgyaniv.github.io/posts/post0001.html");
+//   xhr.responseType = 'document';
+//   xhr.send();
+// }
