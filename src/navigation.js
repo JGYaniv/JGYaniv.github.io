5eac336f645f63cc530f028e74c5e252bb6036d2
Jonathan <yoniyaniv@gmail.com>
2020-03-22 10:14:19 -0400

    makin it classy

--- src/navigation.js
+++ src/navigation.js
@@ -1,47 +1,50 @@
 window.onload = function() {
-  var header = document.getElementById("navigation");
-  var sticky = header.offsetTop;
-  window.onscroll = function() {stickyHeader(sticky, header)};
-  setSelected();
+  Navigation.stickyHeader();
 }
 
-window.onhashchange = function(){
-  setSelected();
+window.onhashchange = function() {
+  Page.setByHash();
+  Navigation.setSelected();
 }
 
-function getPage(page){
-  // event.preventDefault();
-  $.ajax({
-    url: `https://jgyaniv.github.io/${page.slice(1)}.html`,
-    success: function(result){
-      $("#content").html(result);
-    }
-  })
-}
-
-function setSelected(){
-  resetMenu();
-  var page = window.location.hash;
-  var menuItem = document.querySelector(`[href="${page}"]`);
-  if (menuItem.className === 'menu-item') {
-    menuItem.setAttribute("class","selected menu-item");
+class Page {
+  static setByHash(){
+    var currentHash = window.location.hash;
+    $.ajax({
+      url: `https://jgyaniv.github.io/${currentHash.slice(1)}.html`,
+      success: function(result){
+        $("#content").html(result);
+      }
+    })
   }
-  getPage(page);
 }
 
-function resetMenu(){
-  var menuItems = document.querySelectorAll(`[class="selected menu-item"]`);
-  menuItems.forEach(element => element.setAttribute("class", "menu-item"));
-}
+class Navigation{
+  static stickyHeader() {
+    var header = document.getElementById("navigation");
+    var sticky = header.offsetTop;
+    window.onscroll = function() {
+      if (window.pageYOffset >= (sticky-10)) {
+        header.classList.add("sticky");
+        document.getElementById("content").setAttribute("style","margin-top:71px;");
+      } else {
+        header.classList.remove("sticky");
+        document.getElementById("content").setAttribute("style","margin-top:0;");
+      }
+    };
+  }
 
+  static setSelected(){
+    Navigation.resetMenu();
+    var page = window.location.hash;
+    var menuItem = document.querySelector(`[href="${page}"]`);
+    if (menuItem.className === 'menu-item') {
+      menuItem.setAttribute("class","selected menu-item");
+    }
+  }
 
-// keeps header on top of page
-function stickyHeader(sticky, header) {
-  if (window.pageYOffset >= (sticky-10)) {
-    header.classList.add("sticky");
-    document.getElementById("content").setAttribute("style","margin-top:71px;");
-  } else {
-    header.classList.remove("sticky");
-    document.getElementById("content").setAttribute("style","margin-top:0;");
+  static resetMenu(){
+    var menuItems = document.querySelectorAll(`[class="selected menu-item"]`);
+    menuItems.forEach(element => element.setAttribute("class", "menu-item"));
   }
 }
